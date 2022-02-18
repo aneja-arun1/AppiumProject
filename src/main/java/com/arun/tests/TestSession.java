4d3c69b3a687b8ef49383a15b3534762003d5f82
Arun <aneja.arun1@gmail.com>
2022-02-19 00:15:20 +0530

    clicking 'Login Screen', with sleep

--- src/main/java/com/arun/tests/TestSession.java
+++ src/main/java/com/arun/tests/TestSession.java
@@ -2,16 +2,19 @@ package com.arun.tests;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 
 
 public class TestSession {
   private static final String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.9.0/TheApp-v1.9.0.apk";
   private static final String APPIUM = "http://localhost:4723/wd/hub";
+  By loginScreenLink = MobileBy.AccessibilityId("Login Screen"); 
   
   private AndroidDriver driver;
   
@@ -26,12 +29,25 @@ public class TestSession {
     
     driver = new AndroidDriver(new URL(APPIUM), caps);
     
+    try {
+    Thread.sleep(10000);
+    }catch (Exception e){
+      e.printStackTrace();
+    }
   }
   
   @Test
   public void test()
   {
     System.out.println("Inside the test");
+    driver.findElement(loginScreenLink).click();
+    
+    try {
+      Thread.sleep(10000);
+      }catch (Exception e){
+        e.printStackTrace();
+      }
+    
   }
   
   @AfterTest
