8c36ecc5a61265afc9b919b44801ae7cce54f75e
Arun <aneja.arun1@gmail.com>
2022-02-19 01:07:31 +0530

    Adding explicit wait

--- src/main/java/com/arun/tests/TestSession.java
+++ src/main/java/com/arun/tests/TestSession.java
@@ -1,9 +1,12 @@
 package com.arun.tests;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -14,13 +17,16 @@ import io.appium.java_client.android.AndroidDriver;
 public class TestSession {
   private static final String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.9.0/TheApp-v1.9.0.apk";
   private static final String APPIUM = "http://localhost:4723/wd/hub";
-  By loginScreenLink = MobileBy.AccessibilityId("Login Screen"); 
   
   private AndroidDriver driver;
+  private WebDriverWait wait;
+  private By loginScreenLink;
+  
+  private DesiredCapabilities caps;
   
   @BeforeTest
   public void setUp() throws MalformedURLException {
-    DesiredCapabilities caps = new DesiredCapabilities();
+    caps = new DesiredCapabilities();
     caps.setCapability("platformName", "Android");
     caps.setCapability("platformVersion", "9");
     caps.setCapability("deviceName", "Pixel 4 API 28");
@@ -28,12 +34,9 @@ public class TestSession {
     caps.setCapability("app", APP);
     
     driver = new AndroidDriver(new URL(APPIUM), caps);
-    
-    try {
-    Thread.sleep(10000);
-    }catch (Exception e){
-      e.printStackTrace();
-    }
+    wait = new WebDriverWait(driver, 10);
+    loginScreenLink = MobileBy.AccessibilityId("Login Screen");
+    wait.until(ExpectedConditions.presenceOfElementLocated(loginScreenLink));
   }
   
   @Test
@@ -52,7 +55,9 @@ public class TestSession {
   
   @AfterTest
   public void tearDown() {
-    driver.quit();
+    if (driver != null) {
+      driver.quit();
+    }
   }
   
 }
